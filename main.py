41608c6209c1bf941939ba51369d68a02191ca21
alejandro-imt <alejandro.imt1@outlook.com>
2025-01-15 21:39:39 -0600

    Update main

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import numpy as np
+from mpyc.runtime import mpc
 
-a = np.random.randing(0,2,100)
+async def main():
+    mpc.parties = mpc.parties[:3]  # restrict to first three parties
+    await mpc.start()  # connect to all other parties
+    print("".join(await mpc.transfer("Hello world!")))
+    await mpc.shutdown()  # disconnect, but only once all other parties reached this point
 
-print(a)
+# Run the main function
+if __name__ == "__main__":
+    mpc.run(main())
+    
+# Compare this snippet from test_palm_codes.py
+
+
+        
