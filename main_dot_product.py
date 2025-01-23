6b948175f6fb61ecb5759d22a48fed75489c1300
alejandro-imt <alejandro.imt@outlook.com>
2025-01-22 20:46:40 -0600

    Run tests to assess the execution time and commun overhead

--- main_dot_product.py
+++ main_dot_product.py
@@ -2,56 +2,50 @@
 from mpyc.runtime import mpc
 import random
 import numpy as np
+import time
 
 def main():
     # Initialize the MPC
     mpc.run(mpc.start())
     party = mpc.pid
 
-    orig_a = [random.randint(0, 100) for _ in range(1000000)]
-    orig_b = [random.randint(0, 100) for _ in range(1000000)]
+    orig_a = [random.randint(0, 10) for _ in range(260000)]
+    orig_b = [random.randint(0, 10) for _ in range(260000)]
 
+    tic = time.process_time()
+    
     # Secure integer vectors
-    int_a = [mpc.SecInt(7)(a) for a in orig_a]
-    int_b = [mpc.SecInt(7)(b) for b in orig_b]
+    int_a = [mpc.SecInt(16)(a) for a in orig_a]
+    int_b = [mpc.SecInt(16)(b) for b in orig_b]
 
     # Input the secret integer vectors for each party
     sec_a = mpc.input(int_a, senders=0)
-    sec_b = mpc.input(int_b, senders=1)
-
-    # if party == 0:
-    #     print(f"\nOriginal secret vector of P0: {orig_a}")
-    # elif party == 1:
-    #     print(f"\nOriginal secret vector of P1: {orig_b}")
+    sec_b = mpc.input(int_b, senders=0)
 
     # Compute the dot product
     dot_product = mpc.run(mpc.output(mpc.in_prod(sec_a, sec_b)))
+    
+    elapsed_time = time.process_time() - tic
 
     # Reconstructed vectors
-    rec_a = mpc.run(mpc.output(sec_a, receivers=[0, 1]))
-    rec_b = mpc.run(mpc.output(sec_b, receivers=[0, 1]))
-
-    print(f"\nDot product from the secrets: {dot_product}")
-    print("\nReal dot product: ", np.dot(rec_a, rec_b))
-
-    # Reconstructed vectors
-    rec_a = mpc.run(mpc.output(sec_a, receivers=[0, 1]))
-    rec_b = mpc.run(mpc.output(sec_b, receivers=[0, 1]))
+    rec_a = mpc.run(mpc.output(sec_a, receivers=0))
+    rec_b = mpc.run(mpc.output(sec_b, receivers=0))
 
     # Compare the secrets
     if party == 0:
-        if orig_a == rec_a:
-            print("\nSecret vector of P0 is correct.")
-        else:
-            print("\nSecret vector of P0 is incorrect.")
-    if party == 1:
-        if orig_b == rec_b:
-            print("\nSecret vector of P1 is correct.")
-        else:
-            print("\nSecret vector of P1 is incorrect.")
+        real_dot_product = np.dot(rec_a, rec_b)
+        if dot_product != real_dot_product:
+            print("\nERROR")
         
     # Terminate the MPC
     mpc.run(mpc.shutdown())
+    
+    return elapsed_time
 
 if __name__ == "__main__":
-    main()
+    num_tests = 10
+    elapsed_times = []
+    for _ in range(num_tests):
+        elapsed_time = main()
+        elapsed_times.append(elapsed_time)
+    print(f"\nAverage elapsed time: {sum(elapsed_times)/num_tests} seconds")
--- main_manhattan_dist.py
+++ main_manhattan_dist.py
@@ -2,18 +2,21 @@
 from mpyc.runtime import mpc
 import random
 import numpy as np
+import time
 
 def main():
     # Initialize the MPC
     mpc.run(mpc.start())
     party = mpc.pid
 
-    orig_a = [random.randint(0, 4) for _ in range(1000)]
-    orig_b = [random.randint(0, 4) for _ in range(1000)]
+    orig_a = [random.randint(0, 10) for _ in range(130000)]
+    orig_b = [random.randint(0, 10) for _ in range(130000)]
 
+    tic = time.process_time()
+    
     # Secure integer vectors
-    int_a = [mpc.SecInt(8)(a) for a in orig_a]
-    int_b = [mpc.SecInt(8)(b) for b in orig_b]
+    int_a = [mpc.SecInt(16)(a) for a in orig_a]
+    int_b = [mpc.SecInt(16)(b) for b in orig_b]
 
     # Input the secret integer vectors for each party
     sec_a = mpc.input(int_a, senders=0)
@@ -21,47 +24,35 @@ def main():
 
     # Compute the vector differences
     vec_diff = mpc.vector_sub(sec_a, sec_b)
-    
+
     vec_abs = [None] * len(vec_diff)
     for i in range(len(vec_diff)):
         vec_abs[i] = mpc.abs(vec_diff[i])
         
     manhattan_dist = mpc.run(mpc.output(mpc.sum(vec_abs), receivers=0))
     
+    elapsed_time = time.process_time() - tic
+    
+    # Reconstructed vectors
+    rec_a = mpc.run(mpc.output(sec_a, receivers=0))
+    rec_b = mpc.run(mpc.output(sec_b, receivers=0))
+    
+    # Compare the secrets
     if party == 0:
-        print(f"\nManhattan distance from the secrets: {manhattan_dist}")
-        print("\nReal Manhattan distance: ", np.sum(np.abs(np.subtract(orig_a, orig_b))))
-    
-    # Compute the Manhattan distance
-    # manhattan_dist = mpc.run(mpc.sum(mpc.abs(vec_diff)))
-    
-
-    # # Reconstructed vectors
-    # rec_a = mpc.run(mpc.output(sec_a, receivers=[0, 1]))
-    # rec_b = mpc.run(mpc.output(sec_b, receivers=[0, 1]))
-
-    # print(f"\nDot product from the secrets: {dot_product}")
-    # print("\nReal dot product: ", np.dot(rec_a, rec_b))
-
-    # # Reconstructed vectors
-    # rec_a = mpc.run(mpc.output(sec_a, receivers=[0, 1]))
-    # rec_b = mpc.run(mpc.output(sec_b, receivers=[0, 1]))
-
-    # # Compare the secrets
-    # if party == 0:
-    #     if orig_a == rec_a:
-    #         print("\nSecret vector of P0 is correct.")
-    #     else:
-    #         print("\nSecret vector of P0 is incorrect.")
-    # if party == 1:
-    #     if orig_b == rec_b:
-    #         print("\nSecret vector of P1 is correct.")
-    #     else:
-    #         print("\nSecret vector of P1 is incorrect.")
+        real_manhattan_dist = np.sum(np.abs(np.array(rec_a) - np.array(rec_b)))
+        if manhattan_dist != real_manhattan_dist:
+            print("\nERROR")
 
     # Terminate the MPC
     mpc.run(mpc.shutdown())
 
+    return elapsed_time
+
 ########### Main Program ###########
 if __name__ == "__main__":
-    main()
+    num_tests = 5
+    elapsed_times = []
+    for _ in range(num_tests):
+        elapsed_time = main()
+        elapsed_times.append(elapsed_time)
+    print(f"\nAverage elapsed time: {sum(elapsed_times)/num_tests} seconds")
